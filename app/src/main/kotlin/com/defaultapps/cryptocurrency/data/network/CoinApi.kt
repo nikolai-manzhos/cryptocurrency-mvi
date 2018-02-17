2f99d10ddc89b1127230e23f13e5b006f176e18b
NikolayManzhos <NikolayManzhos@users.noreply.github.com>
2018-02-17 21:41:32 +0300

    Add README.md, codestyle fixes in CoinApi

--- app/src/main/kotlin/com/defaultapps/cryptocurrency/data/network/CoinApi.kt
+++ app/src/main/kotlin/com/defaultapps/cryptocurrency/data/network/CoinApi.kt
@@ -2,15 +2,16 @@ package com.defaultapps.cryptocurrency.data.network
 
 import com.defaultapps.cryptocurrency.data.entity.CurrencyResponse
 import io.reactivex.Observable
-import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CoinApi {
     @GET("ticker/")
-    fun getCurrencies(@Query("convert") moneyType: String): Observable<List<CurrencyResponse>>
+    fun getCurrencies(@Query("convert") moneyType: String)
+            : Observable<List<CurrencyResponse>>
 
     @GET("ticker/{id}/")
-    fun getCurrencyDetail(@Path("id") id: String, @Query("convert") moneyType: String): Observable<List<CurrencyResponse>>
+    fun getCurrencyDetail(@Path("id") id: String, @Query("convert") moneyType: String)
+            : Observable<List<CurrencyResponse>>
 }
